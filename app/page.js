6a08148b6a24bb92a6e35a9e31e71f98fda606a1
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-02-02 21:49:38 +0530

    remove img

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import { useAuth } from "./context/usercontext";
 const page = () => {
   const { userStatus } = useAuth();
   const router = useRouter();
-  console.log(userStatus);
   React.useEffect(() => {
     if (userStatus) {
       router.push("/account");
@@ -16,8 +15,8 @@ const page = () => {
 
   return (
     <div className="min-h-screen flex flex-col gap-5 items-center pt-20 w-full text-2xl sm:text-4xl tracking-wide font-semibold">
-      <h1>You are not signed</h1>
-      <h1>Already register? please login</h1>
+      <h2>You are not signed</h2>
+      <h3>Already register? please login</h3>
       <Link href={"/login"}>
         <button className="flex w-full justify-center rounded-md bg-indigo-600 px-6 py-3 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           Log in
